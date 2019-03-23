113e7f7d7a2cc2e9ea8a646d6288afbfd554c386
jatellez <jatellez@miuandes.cl>
2019-03-23 17:14:08 -0300

    *Fix

--- chatanalyzer/util.py
+++ chatanalyzer/util.py
@@ -15,10 +15,12 @@ def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
+
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
+
 
 def echo_all(updates):
     for update in updates["result"]:
@@ -33,16 +35,19 @@ def echo_all(updates):
             print('is exept')
             print(e)
 
+
 def get_json_from_url(url):
     content = get_url(url)
     js = json.loads(content)
     return js
+
 
 def get_last_update_id(updates):
     update_ids = []
     for update in updates["result"]:
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
+
 
 def get_updates(offset=None):
     url = URL + "getUpdates?timeout=100"
@@ -89,11 +94,11 @@ def natural_languaje(message):
 last_update_id = None
 while True:
     updates = get_updates(last_update_id)
-    if len(updates["result"]) > 0:
-        last_update_id = get_last_update_id(updates) + 1
-        echo_all(updates)
-    time.sleep(0.5)
     try:
+        if len(updates) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
         natural_languaje((updates["result"])[0]["message"]["text"])
     except:
-        continue
+        break
