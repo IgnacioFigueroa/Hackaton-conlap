b42a5b7091a67553688394a0ff98983f116e3864
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2019-03-23 16:09:50 -0300

    Added some structure

--- chatanalyzer/models.py
+++ chatanalyzer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Message(models.Model):
+    update_id = models.IntegerField()
+    message_id = models.IntegerField()
+    user_id = models.IntegerField()
+    user_name = models.CharField()
+    user_last = models.CharField()
+    languaje_code = models.CharField()
+    chat_id = models.IntegerField()
+    chat_title = models.IntegerField()
+    type = models.CharField()
+    date = models.IntegerField()
+    text = models.CharField()
 # Create your models here.
