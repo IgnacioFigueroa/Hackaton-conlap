ed31ea6936df6abac0271df87b8f4af6365eeab0
IgnacioFigueroa <ijfigueroa@miuandes.cl>
2019-03-22 22:10:22 -0300

    Changed url of home page Added google charts api

--- HackatonConlap/urls.py
+++ HackatonConlap/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chatanalyzer/', include('chatanalyzer.urls'))
+    path('', include('chatanalyzer.urls'))
 ]
