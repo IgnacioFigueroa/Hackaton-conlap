6db9e02dbd1824580106de7209b1d98bce6391eb
Vicente Correa <vicorrea@miuandes.cl>
2019-03-24 05:55:42 -0300

    Correa push

--- chatanalyzer/util.py
+++ chatanalyzer/util.py
@@ -16,52 +16,52 @@ def get_chart_info(request):
 
 def get_messages_info(request):
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM message")
+    cursor.execute("SELECT * FROM message ORDER BY id")
     rows = cursor.fetchall()
     data = []
     # ----
     predominant_emotions_in_conversation = []
     sums = []
-    mayores = []
     proms = []
-    c = 0
-    f = 0
-    relevant = "I"
+    c, f, mr = 0, 0, 0
 
     for r in rows:
-        more_mayor = False
+        relevant = "I"
         quiebre = False
         mayor_al_promedio = False
-        print(r)
+        #print(r)
         emotions = ["sadness", "joy", "fear", "disgust", "anger"]
-        mayor = max([r[10], r[11], r[12], r[13], r[14]])
-        print("suma de emociones:", sum([r[10], r[11], r[12], r[13], r[14]]))
-        sums.append(sum([r[10], r[11], r[12], r[13], r[14]]))
-        this_emotion = emotions[r.index(mayor) - 10]
-        if (mayor == 0 or mayor == 0.0) and len(predominant_emotions_in_conversation) > 0:
-            this_emotion = predominant_emotions_in_conversation[-1]
-        mayores.append(mayor)
-        proms.append(mean(sums))
-        print("promedio:", proms[-1])
-        if sum([r[10], r[11], r[12], r[13], r[14]]) > proms[-1] - 1:
-            mayor_al_promedio = True
-            if sum([r[10], r[11], r[12], r[13], r[14]]) > proms[-1] + 1:
-                more_mayor = True
-        print(mayor, this_emotion)
-        if len(predominant_emotions_in_conversation) > 0:
-            if this_emotion != predominant_emotions_in_conversation[-1]:
-                quiebre = True
-                print("QUIEBRE")
-        predominant_emotions_in_conversation.append(this_emotion)
-        if mayor_al_promedio:
-            print("---  MENSAJE RELEVANTE  ----")
-            relevant = "R"
-            if more_mayor and quiebre:
-                relevant = "RI"
+        number_emotions = [r[10], r[11], r[12], r[13], r[14]]
+        mayor = max(number_emotions)
+        #print("suma de emociones:", sum(number_emotions))
+
+        if sum(number_emotions) != 0:
+            sums.append(sum(number_emotions))
+            this_emotion = emotions[r.index(mayor) - 10]
+            if (mayor == 0 or mayor == 0.0) and len(predominant_emotions_in_conversation) > 0:
+                this_emotion = predominant_emotions_in_conversation[-1]
+            proms.append(mean(sums))
+            #print("promedio:", proms[-1])
+            if sum(number_emotions) > proms[-1]:
+                mayor_al_promedio = True
+            #print(mayor, this_emotion)
+            if len(predominant_emotions_in_conversation) > 0:
+                if this_emotion != predominant_emotions_in_conversation[-1]:
+                    quiebre = True
+                    #print("QUIEBRE")
+            predominant_emotions_in_conversation.append(this_emotion)
+            if mayor_al_promedio:
+                #print("---  MENSAJE RELEVANTE  ----")
+                relevant = "R"
+                if quiebre:
+                    relevant = "RI"
+                    #print("---  MENSAJE  M U Y   RELEVANTE  ----")
+                    mr += 1
             c += 1
         f += 1
         data.append({"id": r[0], "message_id": r[1], "user_id": r[2], "user_name": r[3], "user_last": r[4],
                      "message_date": r[8], "text": r[9], "emotions_sum": relevant})
-    print("contador:", c, "de", f)
+    #print(proms[-1])
+    print("solo relevantes:", c, "relev y muy relev:", mr, "de un total de", f)
     # ---
     return JsonResponse(data, safe=False)
